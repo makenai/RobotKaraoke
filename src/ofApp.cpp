cf404a5fe62011a9ebec9f32e1fd3f116984eda4
Pawel Szymczykowski <makenai@gmail.com>
2014-06-22 23:04:40 -0700

    Removed breakpoints. Added some code to sync bones to the model (which crashes)

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -8,19 +8,35 @@ void ofApp::setup(){
     openNIDevice.addImageGenerator();
     openNIDevice.addDepthGenerator();
     openNIDevice.setRegister(true);
-    openNIDevice.setMirror(true);
     openNIDevice.addUserGenerator();
-    openNIDevice.setMaxNumUsers(2);
+    openNIDevice.setMaxNumUsers(1);
     openNIDevice.start();
     
     //karaokePlayer.open("/Users/pawels/Documents/OpenFrameworks/apps/myApps/robotKaraoke/data/Queen");
     //karaokePlayer.play();
     
-    model.loadModel("/Users/pawels/Documents/OpenFrameworks/apps/myApps/robotKaraoke/data/models/ED-209/ED-209.dae");
+    model.loadModel("/Users/pawels/Documents/OpenFrameworks/examples/addons/assimpExample/bin/data/astroBoy_walk.dae");
+    //model.loadModel("/Users/pawels/Documents/OpenFrameworks/apps/myApps/robotKaraoke/data/models/ED-209/ED-209.dae");
     //model.loadModel("/Users/pawels/Documents/OpenFrameworks/apps/myApps/robotKaraoke/data/models/H25_humanoid_robot/H25_humanoid_robot.dae");
     
-    //assimpNISync.setup( &model, NULL );
-    //assimpNISync.listBoneNames();
+    assimpNISync.setup( &model, &openNIDevice );
+//    assimpNISync.listBoneNames();
+    
+    assimpNISync.syncBoneAndOpenNIJoint( 0, "head", 1, XN_SKEL_HEAD );
+    assimpNISync.syncBoneAndOpenNIJoint( 1, "head", 1, XN_SKEL_HEAD );
+    assimpNISync.syncBoneAndOpenNIJoint( 2, "head", 1, XN_SKEL_HEAD );
+    assimpNISync.syncBoneAndOpenNIJoint( 3, "head", 1, XN_SKEL_HEAD );
+    assimpNISync.syncBoneAndOpenNIJoint( 0, "neck01", 1, XN_SKEL_NECK );
+    assimpNISync.syncBoneAndOpenNIJoint( 2, "neck01", 1, XN_SKEL_NECK );
+    assimpNISync.syncBoneAndOpenNIJoint( 3, "neck01", 1, XN_SKEL_NECK );
+    assimpNISync.syncBoneAndOpenNIJoint( 3, "L_shoulder", 1, XN_SKEL_LEFT_SHOULDER );
+    assimpNISync.syncBoneAndOpenNIJoint( 3, "R_shoulder", 1, XN_SKEL_RIGHT_SHOULDER );
+    assimpNISync.syncBoneAndOpenNIJoint( 3, "L_elbow", 1, XN_SKEL_LEFT_ELBOW );
+    assimpNISync.syncBoneAndOpenNIJoint( 3, "R_elbow", 1, XN_SKEL_RIGHT_ELBOW );
+    assimpNISync.syncBoneAndOpenNIJoint( 2, "L_wrist", 1, XN_SKEL_LEFT_WRIST );
+    assimpNISync.syncBoneAndOpenNIJoint( 2, "R_wrist", 1, XN_SKEL_RIGHT_WRIST );
+    assimpNISync.syncBoneAndOpenNIJoint( 3, "L_hip", 1, XN_SKEL_LEFT_HIP );
+    assimpNISync.syncBoneAndOpenNIJoint( 3, "R_hip", 1, XN_SKEL_RIGHT_HIP );
     
     light.enable();
     ofEnableDepthTest();
@@ -28,6 +44,9 @@ void ofApp::setup(){
 
 //--------------------------------------------------------------
 void ofApp::update(){
+    
+    openNIDevice.update();
+    //assimpNISync.update();
     
     // ofSoundUpdate();
     // karaokePlayer.update();
@@ -45,14 +64,8 @@ void ofApp::draw(){
 
     model.setPosition(ofGetWidth()/2, (float)ofGetHeight() * 0.75 , 0);
     model.drawFaces();
+    openNIDevice.drawSkeletons();
     
-    
-    int r = ofMap(mouseX, 0, 1024, 0, 255);
-    int b = ofMap(mouseY, 0, 768, 0, 255);
-    ofSetColor( r, 0, b );
-    ofCircle(mouseX, mouseY, 25);
-    
-
 }
 
 //--------------------------------------------------------------
